fbdbc9244a387b9fe6661b188c03a2d14301fa71
Ozeir <0954800@hr.nl>
2020-03-28 21:48:15 +0100

    masterfile2

---
+++ Cinema/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CINEMA HOMESCREEN\n1. Select Movies\n2. Profile (Unavailable)\n3. Reservation\n 4.Information");
+            string c = Console.ReadLine();
+            if (Convert.ToInt32(c) == 1) {
+                Console.WriteLine("**** MOVIE SELECTION ****");
+            }
+        }
+    }
+}
